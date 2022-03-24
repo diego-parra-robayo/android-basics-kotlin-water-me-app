2743bb9e8829fcbd70e629fbd2cd8e6a9787945b
Diego Parra <diego.parra@wizeline.com>
2022-03-24 16:29:11 -0500

    Solution to codelab: https://developer.android.com/codelabs/basic-android-kotlin-training-project-water-me?continue=https%3A%2F%2Fdeveloper.android.com%2Fcourses%2Fpathways%2Fandroid-basics-kotlin-unit-6-pathway-1%23codelab-https%3A%2F%2Fdeveloper.android.com%2Fcodelabs%2Fbasic-android-kotlin-training-project-water-me#4

--- app/src/main/java/com/example/waterme/viewmodel/PlantViewModel.kt
+++ app/src/main/java/com/example/waterme/viewmodel/PlantViewModel.kt
@@ -18,13 +18,20 @@ package com.example.waterme.viewmodel
 import android.app.Application
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
+import androidx.work.ExistingWorkPolicy
+import androidx.work.OneTimeWorkRequestBuilder
+import androidx.work.PeriodicWorkRequestBuilder
 import androidx.work.WorkManager
+import androidx.work.workDataOf
 import com.example.waterme.data.DataSource
+import com.example.waterme.worker.WaterReminderWorker
 import java.util.concurrent.TimeUnit
 
 class PlantViewModel(application: Application): ViewModel() {
 
     val plants = DataSource.plants
+    private val workManager = WorkManager.getInstance(application)
+
 
     internal fun scheduleReminder(
         duration: Long,
@@ -32,11 +39,25 @@ class PlantViewModel(application: Application): ViewModel() {
         plantName: String
     ) {
         // TODO: create a Data instance with the plantName passed to it
+        val data = workDataOf(WaterReminderWorker.nameKey to plantName)
+
 
         // TODO: Generate a OneTimeWorkRequest with the passed in duration, time unit, and data
         //  instance
+        val request = OneTimeWorkRequestBuilder<WaterReminderWorker>()
+            .setInitialDelay(duration, unit)
+            .setInputData(workDataOf(WaterReminderWorker.nameKey to plantName))
+            .build()
+        /*val request = PeriodicWorkRequestBuilder<WaterReminderWorker>(duration, unit)
+            .setInputData(workDataOf(WaterReminderWorker.nameKey to plantName))
+            .build()*/
 
         // TODO: Enqueue the request as a unique work request
+        workManager.enqueueUniqueWork(
+            plantName,
+            ExistingWorkPolicy.REPLACE,
+            request
+        )
     }
 }
 
